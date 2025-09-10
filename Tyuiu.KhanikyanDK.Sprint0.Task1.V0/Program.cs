834daf2551bbda00e3588a0008e1bebb5c90267d
xanik <xanikyan00@gmail.com>
2025-09-10 17:09:02 +0500

    Task1

---
+++ Tyuiu.KhanikyanDK.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KhanikyanDK.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Davo";
+            int age = 18;
+            Console.WriteLine("Hello " + name + "! I am " + age);
+            Console.ReadKey();
+        }
+    }
+}
