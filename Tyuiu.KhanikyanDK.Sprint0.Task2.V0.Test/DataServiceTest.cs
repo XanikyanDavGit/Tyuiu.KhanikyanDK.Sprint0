f4c0a00d8ec55debd6e11f108fa405e4de6cf910
xanik <xanikyan00@gmail.com>
2025-09-10 18:49:58 +0500

    Task2

---
+++ Tyuiu.KhanikyanDK.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.KhanikyanDK.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KhanikyanDK.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Davo";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Davo", res);
+        }
+    }
+}
